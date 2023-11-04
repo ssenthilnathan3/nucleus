6b2f97305f38e69d758c1c4a50c7fc59755c8aef
Senthilnathan <ssenthil1490@outlook.com>
2023-11-04 16:14:51 +0530

    added basic tokenizer

---
+++ build/CMakeFiles/nuclei.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nuclei.dir/src/main.cpp.o"
+  "CMakeFiles/nuclei.dir/src/main.cpp.o.d"
+  "nuclei"
+  "nuclei.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/nuclei.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <optional>
+#include <vector>
+
+enum TokenType {
+    _return,
+    int_lit
+};
+
+struct Token {
+    TokenType type;
+    std::optional<std::string> value{};
+};
+
+std::string token_to_asm(const std::vector<Token>& tokens) {
+    std::stringstream output;
+    output << "global _start\n_start:\n";
+
+    for (int i = 0; i < tokens.size();i++) {
+        const Token& token = tokens.at(i);
+        if (token.type == TokenType::_return) {
+            if (i + 1 < tokens.size() && tokens.at(i + 1).type == TokenType::int_lit) {
+                output << "  mov rax, 60\n";
+                output << "  mov rdi, " << tokens.at(i+1).value.value() << "\n";
+                output << "  syscall";
+            }
+        }
+    }
+    return output.str();
+}
+
+std::vector<Token> tokenize(const std::string& str) {
+    std::vector<Token> tokens;
+    std::string buf;
+    for (int i = 0; i < str.length(); i++) {
+        char c = str.at(i);
+        if (std::isalpha(c)) {
+            buf.push_back(c);
+
+            while (i + 1 < str.length() && std::isalnum(str.at(i + 1))) {
+                i++;
+                buf.push_back(str.at(i));
+            }
+
+            if (buf == "return") {
+                tokens.push_back({.type = TokenType::_return});
+                buf.clear();
+                continue;
+            } else {
+                std::cerr << "You messed up!" << std::endl;
+                exit(EXIT_FAILURE);
+            }
+        } else if (std::isdigit(c)) {
+            buf.push_back(c);
+            while (i + 1 < str.length() && std::isdigit(str.at(i + 1))) {
+                i++;
+                buf.push_back(str.at(i));
+            }
+            tokens.push_back({.type = TokenType::int_lit, .value = buf});
+            buf.clear();
+        } else if (std::isspace(c)) {
+            continue;
+        } else {
+            std::cerr << "You messed up!" << std::endl;
+            exit(EXIT_FAILURE);
+        }
+    }
+    return tokens;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cerr << "Incorrect Usage. Correct usage..." << std::endl;
+        std::cerr << "nuclei <input>" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    std::string contents;
+    {
+        std::stringstream contents_stream;
+        std::fstream input(argv[1], std::ios::in);
+        contents_stream << input.rdbuf();
+        contents = contents_stream.str();
+    }
+
+    std::vector<Token> tokens = tokenize(contents);
+    {
+        std::fstream file("out.asm", std::ios::out);
+        file << token_to_asm(tokens);
+    }
+
+    system("nasm -felf64 out.asm");
+    system("ld -o out out.o");
+    return EXIT_SUCCESS;
+}
